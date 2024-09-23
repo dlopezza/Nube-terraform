7a00ed8c35e8b6b217562e0df464dd2f9fe4a0bc
David Lopez <dlopezza@unal.edu.co>
2024-09-23 01:02:26 -0500

    making the stuff not secure lol

--- ECS/service/security_group.tf
+++ ECS/service/security_group.tf
@@ -4,9 +4,9 @@ resource "aws_security_group" "ecs_service_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]  # Adjust this as needed for security
   }
 
