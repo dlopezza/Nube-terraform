b6c22f89e2744b3d515330d9a75dcc1f34c779a6
David Lopez <dlopezza@unal.edu.co>
2024-09-23 00:33:17 -0500

    changed ecs def

--- ECS/definition/ecs_definition.tf
+++ ECS/definition/ecs_definition.tf
@@ -2,16 +2,30 @@ resource "aws_ecs_task_definition" "ecs_task" {
   family                   = "terraproject_family"
   requires_compatibilities = ["FARGATE"]  # Use "EC2" if not using Fargate
   network_mode             = "awsvpc"      # Required for Fargate
-  container_definitions    = jsonencode([{
-    name      = var.container_name
-    image     = var.image_name
-    memory    = 512
-    cpu       = 256
-    essential = true
-    portMappings = [{
-      containerPort = var.container_port
-      hostPort      = var.host_port
-      protocol      = "tcp"
-    }]
-  }])
+  cpu                      = 1024
+  memory                   = 2048
+
+  container_definitions = <<TASK_DEFINITION
+[
+  {
+    "name": "${var.container_name}",
+    "image": "${var.image_name}",
+    "cpu": 1024,
+    "memory": 2048,
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": ${var.container_port},
+        "hostPort": ${var.host_port},
+        "protocol": "tcp"
+      }
+    ]
+  }
+]
+DEFINITION
+
+  runtime_platform {
+    operating_system_family = "LINUX"  # Update to match your environment (e.g., "WINDOWS_SERVER_2019_CORE" if using Windows)
+    cpu_architecture        = "X86_64"
+  }
 }
