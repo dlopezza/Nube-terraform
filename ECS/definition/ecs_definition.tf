7d30ff504e35de5dbf601585d313e1e33de061de
David Lopez <dlopezza@unal.edu.co>
2024-09-23 22:17:33 -0500

    switched definition back to fargate

--- ECS/definition/ecs_definition.tf
+++ ECS/definition/ecs_definition.tf
@@ -1,9 +1,9 @@
 resource "aws_ecs_task_definition" "ecs_task" {
   family                   = "terraproject_family"
-  requires_compatibilities = ["EC2"]  # Use "EC2" if not using Fargate
-  network_mode             = "awsvpc"  # Required for Fargate
-  cpu                      = 1024
-  memory                   = 2048
+  requires_compatibilities = ["FARGATE"]  # Use "FARGATE" for Fargate launch type
+  network_mode             = "awsvpc"      # Required for Fargate
+  cpu                      = "1024"         # CPU value as a string
+  memory                   = "2048"         # Memory value as a string
 
   container_definitions = <<TASK_DEFINITION
 [
@@ -33,7 +33,7 @@ resource "aws_ecs_task_definition" "ecs_task" {
 TASK_DEFINITION
 
   runtime_platform {
-    operating_system_family = "LINUX"  # Update to match your environment (e.g., "WINDOWS_SERVER_2019_CORE" if using Windows)
+    operating_system_family = "LINUX"  # Update to match your environment
     cpu_architecture        = "X86_64"
   }
 }
