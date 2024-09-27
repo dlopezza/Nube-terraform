ff363d3570b0dff551f2de7146ce859168b0d136
David Lopez <dlopezza@unal.edu.co>
2024-09-26 22:19:55 -0500

    Fixed subnet availability zones, reformatted code

--- main.tf
+++ main.tf
@@ -1,80 +1,70 @@
-resource "aws_vpc" "vpc-terraproject" {
+# Data block to get available availability zones
+data "aws_availability_zones" "available" {}
 
- cidr_block           = "10.0.0.0/16"
+# VPC Resource
+resource "aws_vpc" "vpc_terraproject" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
- enable_dns_hostnames = true
-
- tags = {
-
-   name = "vpc-terraproject"
-
- }
-
+  tags = {
+    Name = "vpc-terraproject"
+  }
 }
 
+# Public Subnet
 resource "aws_subnet" "public_subnet" {
+  vpc_id                = aws_vpc.vpc_terraproject.id  # Use VPC ID from the VPC resource
+  cidr_block            = "10.0.1.0/24"  # CIDR block for the public subnet
+  availability_zone     = data.aws_availability_zones.available.names[0]  # Use the first availability zone
 
- vpc_id                  = aws_vpc.vpc-terraproject.id
+  map_public_ip_on_launch = true
 
- cidr_block              = "10.0.1.0/24"
-
- map_public_ip_on_launch = true
-
- availability_zone       = "us-east-1"
-
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
-
+# Private Subnet
 resource "aws_subnet" "private_subnet" {
+  vpc_id                = aws_vpc.vpc_terraproject.id  # Use VPC ID from the VPC resource
+  cidr_block            = "10.0.2.0/24"  # CIDR block for the private subnet
+  availability_zone     = data.aws_availability_zones.available.names[1]  # Use the second availability zone
 
- vpc_id                  = aws_vpc.vpc-terraproject.id
-
- cidr_block              = "10.0.2.0/24"
-
- availability_zone       = "us-east-1"
-
+  tags = {
+    Name = "private-subnet"
+  }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc_terraproject.id
 
- vpc_id = aws_vpc.vpc-terraproject.id
-
- tags = {
-
-   Name = "internet_gateway"
-
- }
-
+  tags = {
+    Name = "internet_gateway"
+  }
 }
 
+# Route Table
 resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc_terraproject.id
 
- vpc_id = aws_vpc.vpc-terraproject.id
-
- route {
-
-   cidr_block = "0.0.0.0/0"
-
-   gateway_id = aws_internet_gateway.internet_gateway.id
-
- }
-
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
 }
 
-
-
+# Associate Route Table with Public Subnet
 resource "aws_route_table_association" "public_subnet_route" {
-
- subnet_id      = aws_subnet.public_subnet.id
-
- route_table_id = aws_route_table.route_table.id
-
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
 }
 
+# Security Group for ECS Service
 resource "aws_security_group" "ecs_service_sg" {
   name        = "ecs_service_sg"
   description = "Security group for ECS service"
-  vpc_id      = aws_vpc.vpc-terraproject.id
+  vpc_id      = aws_vpc.vpc_terraproject.id
 
   ingress {
     from_port   = 3000
@@ -90,4 +80,3 @@ resource "aws_security_group" "ecs_service_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
